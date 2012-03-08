bd2fca9f2ab7a8385c97e2ac8a38c2ca828b8a9b
unknown <bell@bell-PC.(none)>
2012-03-08 16:56:47 +0900

    test

---
+++ railbook/config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Railbook::Application.config.secret_token = 'e31d36a6bdd566e329947827ad0166abf877df101a82d12b86520928a610e5a65cdefaec17546be4527b6f6108ef906da130c524560ba21baa304dfdbeabc7de'
